ee95a61e8ea27887e268b67ffe199632f321b16b
heejeong <heestory217@ex-em.com>
2022-04-09 13:58:32 +0900

    fix(refactor): 테스트 코드 중복 제거
    -as-is: Calculator 인스턴스 생성 부분 중복
    -to-be: 테스트 메소드 간 실행에 영향을 미치지 않게 하기 위해 @BeforeEach 으로 테스트 메소드 실행 전 Calculator 인스턴스를 초기화한다.

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,27 +10,30 @@ class CalculatorTest {
     * : 첫 번째 인자는 기대하는 결과 값이고, 두 번째 인자는 프로덕션 코드의 메소드를 실행한 결과 값이다.
     * */
 
+    private Calculator cal;
+
+    @BeforeEach
+    void setUp(){
+        cal = new Calculator();
+    }
+
     @Test
     void add() {
-        Calculator cal = new Calculator();
         assertEquals(7, cal.add(3, 4));
     }
 
     @Test
     void subtract() {
-        Calculator cal = new Calculator();
         assertEquals(8, cal.subtract(10, 2));
     }
 
     @Test
     void multiply() {
-        Calculator cal = new Calculator();
         assertEquals(10, cal.multiply(2,5));
     }
 
     @Test
     void divide() {
-        Calculator cal = new Calculator();
         assertEquals(2, cal.divide(8,4));
     }
 }
