2c4729a254c0d12f5e8404ebddadca2784fd5e2f
heejeong <heestory217@ex-em.com>
2022-04-09 13:51:53 +0900

    fix(refactor): 결과 값을 눈이 아닌 프로그램을 통해 자동화
    -as-is: 콘솔에 출력되는 값을 통해 수동으로 확인해야 한다.
    -to-be: 로직을 실행한 후의 결과 값 확인을 프로그래밍을 통해 자동화할 수 있다.

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -4,27 +4,32 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
+    /* AssertEquals.assertEquals(expected, actual);
+    * : assertEquals두애는 import static으로 메소드를 import한 후 구현할 수 있다.
+    * : 첫 번째 인자는 기대하는 결과 값이고, 두 번째 인자는 프로덕션 코드의 메소드를 실행한 결과 값이다.
+    * */
+
     @Test
     void add() {
         Calculator cal = new Calculator();
-        System.out.println(cal.add(3,4));
+        assertEquals(7, cal.add(3, 4));
     }
 
     @Test
     void subtract() {
         Calculator cal = new Calculator();
-        System.out.println(cal.subtract(10, 2));
+        assertEquals(8, cal.subtract(10, 2));
     }
 
     @Test
     void multiply() {
         Calculator cal = new Calculator();
-        System.out.println(cal.multiply(2,5));
+        assertEquals(10, cal.multiply(2,5));
     }
 
     @Test
     void divide() {
         Calculator cal = new Calculator();
-        System.out.println(cal.divide(8,4));
+        assertEquals(2, cal.divide(8,4));
     }
 }
